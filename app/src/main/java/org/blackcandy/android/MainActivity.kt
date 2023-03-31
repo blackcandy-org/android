1b6f03bd545361133247a04c7dde73ce5c050385
aidewoode <aidewoode@gmail.com>
2023-03-31 16:50:11 +0800

    Add native bottom nav

--- app/src/main/java/org/blackcandy/android/MainActivity.kt
+++ app/src/main/java/org/blackcandy/android/MainActivity.kt
@@ -1,17 +1,47 @@
 package org.blackcandy.android
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.ViewFlipper
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView.OnItemSelectedListener
 import dev.hotwire.turbo.activities.TurboActivity
 import dev.hotwire.turbo.delegates.TurboActivityDelegate
+import dev.hotwire.turbo.session.TurboSessionNavHostFragment
 
-class MainActivity : AppCompatActivity(), TurboActivity {
+class MainActivity : AppCompatActivity(), TurboActivity, OnItemSelectedListener {
     override lateinit var delegate: TurboActivityDelegate
+    lateinit var homeNav: TurboSessionNavHostFragment
+    lateinit var libraryNav: TurboSessionNavHostFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        delegate = TurboActivityDelegate(this, R.id.main_nav_host)
+        delegate = TurboActivityDelegate(this, R.id.home_nav_host)
+        homeNav = delegate.registerNavHostFragment(R.id.home_nav_host)
+        libraryNav = delegate.registerNavHostFragment(R.id.library_nav_host)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNav.setOnItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val tabSwitcher = findViewById<ViewFlipper>(R.id.nav_switcher)
+
+        return when (item.itemId) {
+            R.id.nav_menu_home -> {
+                delegate.currentNavHostFragmentId = homeNav.id
+                tabSwitcher.displayedChild = 0
+                true
+            }
+            R.id.nav_menu_library -> {
+                delegate.currentNavHostFragmentId = libraryNav.id
+                tabSwitcher.displayedChild = 1
+                true
+            }
+            else -> false
+        }
     }
 }
