9d05338078970ff100efb4ed52ffde09af79f096
aidewoode <aidewoode@gmail.com>
2023-04-04 13:51:47 +0800

    Refactor NavHostFragment

--- app/src/main/java/org/blackcandy/android/HomeNavHostFragment.kt
+++ app/src/main/java/org/blackcandy/android/HomeNavHostFragment.kt
@@ -1,36 +1,5 @@
 package org.blackcandy.android
 
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import dev.hotwire.turbo.config.TurboPathConfiguration
-import dev.hotwire.turbo.session.TurboSessionNavHostFragment
-import org.blackcandy.android.features.web.WebFragment
-import kotlin.reflect.KClass
-
-class HomeNavHostFragment : TurboSessionNavHostFragment() {
+class HomeNavHostFragment : MainNavHostFragment() {
     override val sessionName = "home"
-
-    override val startLocation = "http://10.0.2.2:3000"
-
-    override val registeredActivities: List<KClass<out AppCompatActivity>>
-        get() = listOf(
-            // Leave empty unless you have more
-            // than one TurboActivity in your app
-        )
-
-    override val registeredFragments: List<KClass<out Fragment>>
-        get() = listOf(
-            WebFragment::class,
-            // And any other TurboFragments in your app
-        )
-
-    override val pathConfigurationLocation: TurboPathConfiguration.Location
-        get() = TurboPathConfiguration.Location(
-            assetFilePath = "json/configuration.json",
-        )
-
-    override fun onSessionCreated() {
-        super.onSessionCreated()
-        session.webView.settings.userAgentString = "Turbo Native Android"
-    }
 }
---
+++ app/src/main/java/org/blackcandy/android/MainNavHostFragment.kt
@@ -0,0 +1,35 @@
+package org.blackcandy.android
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import dev.hotwire.turbo.config.TurboPathConfiguration
+import dev.hotwire.turbo.session.TurboSessionNavHostFragment
+import org.blackcandy.android.features.web.WebFragment
+import kotlin.reflect.KClass
+
+open class MainNavHostFragment : TurboSessionNavHostFragment() {
+    override val sessionName = "main"
+    override val startLocation = "http://10.0.2.2:3000"
+
+    override val registeredActivities: List<KClass<out AppCompatActivity>>
+        get() = listOf(
+            // Leave empty unless you have more
+            // than one TurboActivity in your app
+        )
+
+    override val registeredFragments: List<KClass<out Fragment>>
+        get() = listOf(
+            WebFragment::class,
+            // And any other TurboFragments in your app
+        )
+
+    override val pathConfigurationLocation: TurboPathConfiguration.Location
+        get() = TurboPathConfiguration.Location(
+            assetFilePath = "json/configuration.json",
+        )
+
+    override fun onSessionCreated() {
+        super.onSessionCreated()
+        session.webView.settings.userAgentString = "Turbo Native Android"
+    }
+}
