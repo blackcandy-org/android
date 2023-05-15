cc06c8375e9b280afcc4e95ae9ecf37f42c09abe
aidewoode <aidewoode@gmail.com>
2023-05-15 16:45:50 +0800

    Add account menu icon to the top bar of home page

--- app/src/main/java/org/blackcandy/android/features/web/WebHomeFragment.kt
+++ app/src/main/java/org/blackcandy/android/features/web/WebHomeFragment.kt
@@ -1,6 +1,19 @@
 package org.blackcandy.android.features.web
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import dev.hotwire.turbo.nav.TurboNavGraphDestination
+import org.blackcandy.android.databinding.FragmentWebHomeBinding
 
 @TurboNavGraphDestination(uri = "turbo://fragment/web/home")
-class WebHomeFragment : WebFragment()
+class WebHomeFragment : WebFragment() {
+    private var _binding: FragmentWebHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentWebHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
