2e481163cb018666ea47497898567b84f25f429c
aidewoode <aidewoode@gmail.com>
2023-05-26 13:58:22 +0800

    Fix #5, handle modal view

---
+++ app/src/main/java/org/blackcandy/android/fragments/web/WebBottomSheetFragment.kt
@@ -0,0 +1,7 @@
+package org.blackcandy.android.fragments.web
+
+import dev.hotwire.turbo.fragments.TurboWebBottomSheetDialogFragment
+import dev.hotwire.turbo.nav.TurboNavGraphDestination
+
+@TurboNavGraphDestination(uri = "turbo://fragment/web/bottom_sheet")
+class WebBottomSheetFragment : TurboWebBottomSheetDialogFragment()
