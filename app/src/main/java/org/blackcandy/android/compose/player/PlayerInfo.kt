b8da3303cd7fbe9706b8a3b26cd4cefd00c3b69a
aidewoode <aidewoode@gmail.com>
2023-11-21 16:01:13 +0800

    Add static view for player info.

---
+++ app/src/main/java/org/blackcandy/android/compose/player/PlayerInfo.kt
@@ -0,0 +1,54 @@
+package org.blackcandy.android.compose.player
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import coil.compose.AsyncImage
+import org.blackcandy.android.R
+
+@Composable
+fun PlayerInfo(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier,
+    ) {
+        AsyncImage(
+            model = "",
+            contentDescription = stringResource(R.string.album_cover),
+            modifier =
+                Modifier
+                    .widthIn(max = dimensionResource(R.dimen.player_album_cover_max_size))
+                    .fillMaxWidth()
+                    .aspectRatio(1f)
+                    .clip(RoundedCornerShape(dimensionResource(R.dimen.corner_radius_medium)))
+                    .background(color = MaterialTheme.colorScheme.onPrimaryContainer),
+            contentScale = ContentScale.FillWidth,
+        )
+
+        Text(
+            stringResource(R.string.not_playing),
+            modifier =
+                Modifier
+                    .padding(top = dimensionResource(R.dimen.padding_medium)),
+            style = MaterialTheme.typography.titleLarge,
+        )
+
+        Text(
+            "Artist Name",
+            style = MaterialTheme.typography.titleMedium,
+        )
+    }
+}
--- app/src/main/java/org/blackcandy/android/compose/player/PlayerScreen.kt
+++ app/src/main/java/org/blackcandy/android/compose/player/PlayerScreen.kt
@@ -1,9 +1,21 @@
 package org.blackcandy.android.compose.player
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import org.blackcandy.android.R
 
 @Composable
 fun PlayerScreen() {
-    Text(text = "PlayerScreen")
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier =
+            Modifier
+                .padding(horizontal = dimensionResource(R.dimen.padding_medium)),
+    ) {
+        PlayerInfo()
+    }
 }
