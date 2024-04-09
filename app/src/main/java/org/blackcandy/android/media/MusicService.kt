aaa869f55e1207f75620afb702d927682df43c00
aidewoode <aidewoode@gmail.com>
2024-04-09 15:32:50 +0800

    Enable audio offload

--- app/src/main/java/org/blackcandy/android/media/MusicService.kt
+++ app/src/main/java/org/blackcandy/android/media/MusicService.kt
@@ -1,9 +1,13 @@
 package org.blackcandy.android.media
 
 import android.content.Intent
+import androidx.annotation.OptIn
+import androidx.media3.common.TrackSelectionParameters
+import androidx.media3.common.util.UnstableApi
 import androidx.media3.datasource.DataSource
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.source.DefaultMediaSourceFactory
+import androidx.media3.exoplayer.trackselection.DefaultTrackSelector
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
 import org.koin.android.ext.android.get
@@ -11,6 +15,7 @@ import org.koin.android.ext.android.get
 class MusicService : MediaSessionService() {
     private var mediaSession: MediaSession? = null
 
+    @OptIn(UnstableApi::class)
     override fun onCreate() {
         super.onCreate()
 
@@ -21,7 +26,21 @@ class MusicService : MediaSessionService() {
                 .setMediaSourceFactory(
                     DefaultMediaSourceFactory(this).setDataSourceFactory(dataSourceFactory),
                 )
+                .setTrackSelector(
+                    DefaultTrackSelector(this).apply {
+                        setParameters(
+                            buildUponParameters().apply {
+                                setAudioOffloadPreferences(
+                                    TrackSelectionParameters.AudioOffloadPreferences.DEFAULT.buildUpon().apply {
+                                        setAudioOffloadMode(TrackSelectionParameters.AudioOffloadPreferences.AUDIO_OFFLOAD_MODE_ENABLED)
+                                    }.build(),
+                                )
+                            },
+                        )
+                    },
+                )
                 .build()
+
         mediaSession = MediaSession.Builder(this, player).build()
     }
 
